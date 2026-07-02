f8cc78c1e7f04817f97e029c02a92c02f118f671
AydanKerim <apartaliyeva@gmail.com>
2026-07-02 12:25:25 +0400

    dashboard elave edildi

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -4,6 +4,38 @@
 
 <h1>Dashboard</h1>
 
-<p>Welcome Admin</p>
+<div class="dashboard">
+
+    <div class="dashboard-card">
+        <h3>Kurslar</h3>
+        <span>{{ \App\Models\Course::count() }}</span>
+    </div>
+
+    <div class="dashboard-card">
+        <h3>Təlimçilər</h3>
+        <span>{{ \App\Models\Trainer::count() }}</span>
+    </div>
+
+    <div class="dashboard-card">
+        <h3>Məzunlar</h3>
+        <span>{{ \App\Models\Graduate::count() }}</span>
+    </div>
+
+    <div class="dashboard-card">
+        <h3>Partnyorlar</h3>
+        <span>{{ \App\Models\Partner::count() }}</span>
+    </div>
+
+    <div class="dashboard-card">
+        <h3>Məqalələr</h3>
+        <span>{{ \App\Models\Article::count() }}</span>
+    </div>
+
+    <div class="dashboard-card">
+        <h3>FAQ</h3>
+        <span>{{ \App\Models\Faq::count() }}</span>
+    </div>
+
+</div>
 
 @endsection
